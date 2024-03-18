1eb4d0e611a7e3b241440ac585e9bed34048d42b
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-03-18 03:33:48 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "200.39.120.45",
+  host: "200.39.120.45:8080",
   user: "root",
   password: "",
   port: 3306,
