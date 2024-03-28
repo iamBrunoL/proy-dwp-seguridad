e84fabb1a27a3b30983870448dc272eeb71bd77e
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-03-28 08:01:44 -0600

    Add files via upload

---
+++ src/database/proy-dwp.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-03-2024 a las 15:00:26
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proy-dwp`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `datos` ()   BEGIN
+    SELECT c.id, u.username, u.telefono, c.motivo, c.descripcion, c.lugar, c.fechaProgramada, c.estatus
+    FROM usuarios u
+    JOIN citas c ON u.id = c.id_usuario;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `id` int(11) NOT NULL,
+  `nombreCompleto` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `telefono` bigint(11) NOT NULL,
+  `domicilio` varchar(100) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `role` enum('Administrador','Supervisor') NOT NULL,
+  `fechaRegistro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`id`, `nombreCompleto`, `apellidos`, `username`, `email`, `password`, `telefono`, `domicilio`, `fechaNacimiento`, `role`, `fechaRegistro`) VALUES
+(1, 'Luis Gerardo', 'Bruno López', 'eve', 'eve@gmail.com', '1', 1111111111, 'Hgghy gygg', '2024-01-01', 'Administrador', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id` int(11) NOT NULL,
+  `fechaRegistro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_usuario` int(11) NOT NULL,
+  `motivo` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `lugar` enum('Sin asignar',' Miguel Hidalgo #12','Cruz Díaz #14 B','Ignacio Nuñez #114') NOT NULL,
+  `fechaProgramada` date NOT NULL,
+  `estatus` enum('Pendiente','Cancelado','Efectuado') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`id`, `fechaRegistro`, `id_usuario`, `motivo`, `descripcion`, `lugar`, `fechaProgramada`, `estatus`) VALUES
+(1, '2024-03-05 06:39:21', 1, 'Reunión de trabajo', 'sadas dasd sad ', 'Sin asignar', '2024-03-26', 'Cancelado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reportes`
+--
+
+CREATE TABLE `reportes` (
+  `id` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombreCompleto` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `telefono` bigint(11) NOT NULL,
+  `domicilio` varchar(100) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `role` varchar(255) NOT NULL DEFAULT 'Usuario general',
+  `fechaRegistro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombreCompleto`, `apellidos`, `username`, `email`, `password`, `telefono`, `domicilio`, `fechaNacimiento`, `role`, `fechaRegistro`) VALUES
+(1, 'Evelin Guadalupe', 'Delgado Contreras', 'evelin', 'eve123@gmail.com', '2', 1234567890, 'Hgghy', '2024-01-01', 'Usuario general', '0000-00-00');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`,`email`,`telefono`);
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `reportes`
+--
+ALTER TABLE `reportes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`,`telefono`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `citas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
