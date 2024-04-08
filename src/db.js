ee441d7047dd743409cda87870d243da0364fd24
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-04-07 20:19:58 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "monorail.proxy.rlwy.net",
-  user: "root",
-  password: "THlTMvLscxsrzJAtqAcUVvKAFnfZmkIn",
-  port: 42254,
-  database: "railway",
+  host: "blao5xqfq9hfx2xk7rgf-mysql.services.clever-cloud.com",
+  user: "u1vuve184jrusyc2",
+  password: "ik3esLZEGNz9M3Iys0T3",
+  port: 3306,
+  database: "blao5xqfq9hfx2xk7rgf",
 });
