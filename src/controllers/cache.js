b1d559fa006aab30f1082d1b7c91573ee686842f
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-04-09 11:00:35 -0600

    Add files via upload

---
+++ src/controllers/cache.js
@@ -0,0 +1,7 @@
+export const setNoCacheHeaders = (req, res, next) => {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    next();
+  };
+  
