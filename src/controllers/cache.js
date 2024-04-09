db9f6685a542e264cd659f682c3abcbd389cec97
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-04-09 10:58:27 -0600

    Delete src directory

--- src/controllers/cache.js
+++
@@ -1,7 +0,0 @@
-export const setNoCacheHeaders = (req, res, next) => {
-    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.setHeader("Pragma", "no-cache");
-    res.setHeader("Expires", "0");
-    next();
-  };
-  
