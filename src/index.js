865ae85a9c0e0e8ee8b069d544683e737c904b11
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-04-10 10:19:39 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import app from "./app.js";
 import { port } from "./config.js";
 
 app.listen(port);
-console.log(`server on port ${port}`);
-
-// Registro de log
-   let crearLog = `Despliegue de aplicación (Sapphire Networks) a las ${new Date().toLocaleString()}`;
-   pool.query("INSERT INTO reportes (contenido) values (?)", [crearLog]);
+console.log(`Despliegue en el puerto ${port} del servidor`);
