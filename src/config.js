456d70fa66f10c4f5d97904b36abc65796b39c7d
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-04-10 10:45:30 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,1 @@
 export const port = process.env.PORT || 3000;
-
-
-// Registro de log
-   let crearLog = `Despliegue de aplicación (Sapphire Networks) a las ${new Date().toLocaleString()}`;
-   pool.query("INSERT INTO reportes (contenido) values (?)", [crearLog]);
