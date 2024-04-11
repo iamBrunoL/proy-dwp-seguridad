2314c1a4c93f18b224f91790bdf215ddd89f2b4f
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-04-11 10:45:36 -0600

    Update usuario.routes.js

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { cerrarSesion, generarPDFPersonal, iniciarSesion, renderConsultaPersonal, renderConsultaUsuarios, createUsuariosPersonal, renderPersonal, updateUsuarioRole, renderModificarRolUsuarios } from "../controllers/usuarioController.js";
+import { cerrarSesion, generarPDFPersonal, iniciarSesion, renderConsultaPersonal, renderConsultaUsuarios, createUsuariosPersonal, renderPersonal, updateUsuarioRole } from "../controllers/usuarioController.js";
 import { verificarAutenticacion, verificarRol } from "../controllers/authMiddleware.js";
 import { setNoCacheHeaders } from "../controllers/cache.js";
 import { renderMisDatos, buscarUsuario, generarPDFUsuarios } from "../controllers/usuarioController.js";
@@ -37,7 +37,6 @@ router.post("/addUsuario", createUsuarios);
 router.get("/personal", setNoCacheHeaders, verificarAutenticacion, verificarRol("Supervisor"), renderPersonal);
 router.post("/addPersonal", setNoCacheHeaders, verificarAutenticacion, verificarRol("Supervisor"), createUsuariosPersonal);
 router.post("/updateUsuarioRole/:id", setNoCacheHeaders, verificarAutenticacion, verificarRol("Supervisor"), updateUsuarioRole);
-router.get("/modificarRolUsuarios/", setNoCacheHeaders, verificarAutenticacion, verificarRol("Supervisor"), renderModificarRolUsuarios);
 
 router.get('/generarPDFUsuarios', setNoCacheHeaders, verificarAutenticacion, verificarRol("Supervisor"), generarPDFUsuarios);
 router.get('/generarPDFPersonal', setNoCacheHeaders, verificarAutenticacion, verificarRol("Supervisor"), generarPDFPersonal);
